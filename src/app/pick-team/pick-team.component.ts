d44488c35388fa2327dc1087354931ffbbdc4911
AhmedYasser97 <ahmed.yasserelsherif@student.guc.edu.eg>
2018-12-03 02:22:58 +0200

    Progress

--- src/app/pick-team/pick-team.component.ts
+++ src/app/pick-team/pick-team.component.ts
@@ -25,9 +25,57 @@ export class PickTeamComponent implements OnInit {
     }
   ];
 
+  array2 = [
+    {
+      guid: '900ea552-ef68-42cc-b6a6-b8c4dff10fb7',
+      age: 32,
+      name: 'L'
+    },
+    {
+      guid: '880381d3-8dca-4aed-b207-b3b4e575a15f',
+      age: 25,
+      name: 'M'
+    },
+    {
+      guid: '87b47684-c465-4c51-8c88-3f1a1aa2671b',
+      age: 32,
+      name: 'N'
+    }
+  ];
+
+  array3 = [
+    {
+      guid: '900ea552-ef68-42cc-b6a6-b8c4dff10fb7',
+      age: 32,
+      name: 'O'
+    },
+    {
+      guid: '880381d3-8dca-4aed-b207-b3b4e575a15f',
+      age: 25,
+      name: 'P'
+    },
+    {
+      guid: '87b47684-c465-4c51-8c88-3f1a1aa2671b',
+      age: 32,
+      name: 'Q'
+    }
+  ];
+
+  //indexOfarray2: int;
+  public show: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  chooseTeam(element){
+  	// for (var i = 0 ; i < array2.length; i++) {
+  	// 	if((element.age)==array2[i].age){
+  	// 		indexOfarray2 = i;
+  	// 	}
+  	// }
+  	this.show=!this.show;
   }
 
 }
